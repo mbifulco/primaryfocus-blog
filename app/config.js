ca57fd897323c4441e1562f4a4b8af683b8d2cee
Mike Bifulco <mbifulco@live.com>
2022-01-30 12:15:20 -0500

    add socials to config, http before localhost

--- app/config.js
+++ app/config.js
@@ -1,6 +1,13 @@
 const config = {
   // Your website's name, used for favicons, app name/title, and app notifications.
-  siteUrl: process.env.SITE_URL || 'localhost:3000',
+  siteUrl:
+    typeof window !== 'undefined'
+      ? window.ENV.SITE_URL || 'http://localhost:3000'
+      : process.env.SITE_URL || 'http://localhost:3000',
+  social: {
+    youTube: 'https://youtube.com/c/primaryfocus',
+    instagram: 'https://instagram.com/primaryfocus_',
+  },
 };
 
 export default config;
