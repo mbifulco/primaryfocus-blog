226933a041ec2982d7eab411b90a6ddfd8b7390d
Mike Bifulco <mbifulco@live.com>
2022-01-30 12:16:14 -0500

    create sitemap

---
+++ app/routes/sitemap[.]xml.jsx
@@ -0,0 +1,75 @@
+import { format } from 'date-fns';
+
+import config from '~/config';
+import { getClient } from '~/lib/sanity/getClient';
+
+export const fullUrl = (url) => {
+  if (url.startsWith('http')) {
+    return url;
+  }
+
+  if (url.startsWith('/')) {
+    return `${config.siteUrl}${url}`;
+  }
+
+  return `${config.siteUrl}/${url}`;
+};
+
+export const SitemapUrl = ({
+  frequency = 'daily',
+  priority = 1.0,
+  url,
+  lastModified = new Date(),
+}) => {
+  if (!url) return null;
+
+  // format date as yyyy-mm-dd
+  const formattedLastModified = format(lastModified, 'yyyy-MM-dd');
+
+  return `<url>
+    <loc>${fullUrl(url)}</loc>
+    <lastmod>${formattedLastModified}</lastmod>
+    <changefreq>${frequency}</changefreq>
+    <priority>${priority}</priority>
+  </url>`;
+};
+
+export async function loader({ params }) {
+  const staticUrls = ['/', '/about', '/contact', '/articles', '/tags'];
+
+  const posts = await getClient().fetch(
+    `*[_type == "post"]{
+      slug,
+      publishedAt,
+      "tags": tags[]->{title, slug}
+    } `,
+  );
+
+  const tags = await getClient().fetch(
+    `*[_type == "tag"]{
+      slug,
+      title
+    } `,
+  );
+
+  console.log('tags', tags);
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticUrls.map((url) => SitemapUrl({ url })).join('\n')}
+    ${posts
+      .map((post) => SitemapUrl({ url: `/articles/${post?.slug?.current}` }))
+      .join('\n')}
+    ${tags
+      .map((tag) => SitemapUrl({ url: `/tags/${tag?.slug?.current}` }))
+      .join('\n')}
+      
+  </urlset>
+  `;
+  return new Response(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
